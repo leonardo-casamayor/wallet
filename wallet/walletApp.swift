86179fd11146e2f8264950c3a4262dcf368bd407
leonardo.casamayor <leonardo.casamayor@globant.com>
2022-04-04 14:45:09 -0300

    Initial commit

---
+++ wallet/walletApp.swift
@@ -0,0 +1,17 @@
+//
+//  walletApp.swift
+//  wallet
+//
+//  Created by Leonardo Casamayor on 04/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct walletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
